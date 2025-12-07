440b92f90433a8c186245dd2750987ef66bb915d
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-07 20:17:28 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,19 +1,32 @@
 import motor.motor_asyncio
-from info import DATABASE_URI
+from info import DATABASE_URI, DATABASE_NAME
 
-class Database:
+class UserChatDB:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.users
-
-    async def get_banned(self):
-        # Dummy function to prevent errors initially
-        return [], []
+        self.users = self.db.users
+        self.groups = self.db.groups
 
     async def add_user(self, id):
-        user = await self.col.find_one({'id': int(id)})
+        """User ko database me add karta hai"""
+        user = await self.users.find_one({'id': int(id)})
         if not user:
-            await self.col.insert_one({'id': int(id)})
+            await self.users.insert_one({'id': int(id)})
 
-db = Database(DATABASE_URI, "MyBotDB")
+    async def add_group(self, id):
+        """Group ko database me add karta hai"""
+        group = await self.groups.find_one({'id': int(id)})
+        if not group:
+            await self.groups.insert_one({'id': int(id)})
+
+    async def total_users_count(self):
+        """Total Users ginta hai"""
+        return await self.users.count_documents({})
+
+    async def total_groups_count(self):
+        """Total Groups ginta hai"""
+        return await self.groups.count_documents({})
+
+# Database Object
+db = UserChatDB(DATABASE_URI, DATABASE_NAME)

beb70c91df1302555d35b693b7952ccef4f7a7f3
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-09 00:54:50 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,7 @@
+class script(object):
+    # 👇 Is Footer ko apne hisab se edit karein
+    CUSTOM_FOOTER = """
+-----------------------------------------
+<b>⚡ Powered by @VPFILS</b>
+<b>✨ Join for More: @YourChannel</b>
+    """

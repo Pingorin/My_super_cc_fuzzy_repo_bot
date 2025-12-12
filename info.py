636ce001687ea9745111b3a69d06814af8e16f05
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-12 11:44:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,3 +15,9 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0")) # Agar log channel nahi ha
 PORT = int(os.environ.get("PORT", "8080"))
 # Channel ID jahan files upload hoti hain (Multiple IDs comma laga kar daal sakte hain)
 CHANNELS = [-1003342421845] 
+
+# --- VERIFICATION SYSTEM ---
+IS_VERIFY = True # Ise False karein agar verify system band karna ho
+VERIFY_EXPIRE = 24 * 60 * 60 # 24 Hours (Seconds me)
+SHORTLINK_URL = "softurl.in" # Default Website
+SHORTLINK_API = "613ce973446725bfe2bf909b320c7a1e84c4bdc8" # Default API Key

c9807e48bf3d00b89b899cfe51d232f74decb80b
Pingorin <125970228+Pingorin@users.noreply.github.com>
2026-04-29 01:35:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,20 +5,20 @@ import re
 id_pattern = re.compile(r'^-?\d+$')
 
 # --- MANDATORY VARIABLES ---
-api_id_env = os.environ.get("API_ID", "")
+api_id_env = os.environ.get("API_ID", "20638104")
 API_ID = int(api_id_env) if api_id_env else 0
 
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_HASH = os.environ.get("API_HASH", "6c884690ca85d39a4c5ad7c15b194e42")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8527539919:AAFFu37tTXtn7PQS0ioJj4lVlEVgQT3Cr5A")
 
 # 🔥 NAYA FEATURE: Redirect Search Results to Another Bot (Bot B)
 # Yahan Bot B (File Store Bot) ka username daalein bina '@' lagaye. E.g., "MyMovieFileBot"
 FILE_STORE_BOT = os.environ.get("FILE_STORE_BOT", "")
 
-TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "") # Default Test Key
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "9e1353ccc623e71f80262309cda5cdfb") # Default Test Key
 
 # --- DATABASE SETTINGS ---
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://ronak99:ronak99@cluster0.xxbkgsd.mongodb.net/?retryWrites=true&w=majority&minPoolSize=1&maxPoolSize=20")
 
 # 🔥 NEW: MULTI-DATABASE ARCHITECTURE SETTINGS 🔥
 # Jab DB 1 full ho jaye, toh host (Render/Heroku) ke variables me inki link daal dena
@@ -30,16 +30,16 @@ INDEX_DB = int(index_db_env) if index_db_env else 1
 
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Telegram_files')
-USER_DB_URI = os.environ.get("USER_DB_URI", "")
+USER_DB_URI = os.environ.get("USER_DB_URI", "mongodb+srv://Priya55:Priya55@cluster0.nmxnzme.mongodb.net/?retryWrites=true&w=majority&minPoolSize=1&maxPoolSize=20")
 if not USER_DB_URI:
     USER_DB_URI = DATABASE_URI
   
 # --- GENERAL SETTINGS ---
 # 🔥 FIX: Ab Admin IDs bina environment variable ke bhi perfectly work karegi
-ADMINS_STR = os.environ.get("ADMINS", "") # Yahan "" ke andar default admin IDs dal sakte hain space se separate karke (e.g., "123456789 987654321")
+ADMINS_STR = os.environ.get("ADMINS", "7245547751") # Yahan "" ke andar default admin IDs dal sakte hain space se separate karke (e.g., "123456789 987654321")
 ADMINS = [int(i) for i in ADMINS_STR.split()] if ADMINS_STR else []
 
-log_env = os.environ.get("LOG_CHANNEL", "")
+log_env = os.environ.get("LOG_CHANNEL", "-1003474604893")
 LOG_CHANNEL = int(log_env) if log_env else 0
 
 port_env = os.environ.get("PORT", "8080")
@@ -58,7 +58,7 @@ BIN_CHANNEL = int(bin_env) if bin_env else 0
 
 # 🌟 AUTO-POSTER SETTINGS 🌟
 # Channel jahan TMDB se movie posters auto-post honge
-updates_channel_env = os.environ.get("UPDATES_CHANNEL", "")
+updates_channel_env = os.environ.get("UPDATES_CHANNEL", "-1003911004326")
 UPDATES_CHANNEL = int(updates_channel_env) if updates_channel_env and id_pattern.search(updates_channel_env) else None
 
 # Agar Auto-poster dusre bot se chalwana hai, toh uska token yahan dalein. Main bot se chalwana ho toh khali chhod dein.
